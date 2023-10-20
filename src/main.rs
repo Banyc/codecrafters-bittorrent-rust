9b3051dbbcdf58e029fb2a211ae6c464c124de87
Banyc <36535895+Banyc@users.noreply.github.com>
2023-10-20 09:51:18 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json;
+// use serde_json;
 use std::env;
 
 // Available if you need it!
@@ -7,16 +7,26 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    if encoded_value.chars().next().unwrap().is_ascii_digit() {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
     }
+
+    // If encoded_value starts with 'i', it's an integer
+    if encoded_value.starts_with('i') {
+        // Example: "i52e" -> 52
+        // Example: "i-52e" -> -52
+        let e_index = encoded_value.find('e').unwrap();
+        let integer_string = &encoded_value[1..e_index];
+        let integer = integer_string.parse::<i64>().unwrap();
+        return serde_json::Value::Number(integer.into());
+    }
+
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
